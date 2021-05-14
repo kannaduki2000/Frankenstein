7c56c727f4d0d953914b79fed2660596d97130a9
tech <tech@tech>
2021-05-14 18:54:15 +0900

    電流関連

--- Frankenstein/Assets/WorkFolder/yoshidatomoya/Scripts/ElectricCurrent.cs
+++ Frankenstein/Assets/WorkFolder/yoshidatomoya/Scripts/ElectricCurrent.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class ElectricCurrent : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float speed;
+    private Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // プレイヤー移動
+        float horizontalKey = Input.GetAxis("Horizontal");
+
+        if(horizontalKey > 0)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        else if (horizontalKey < 0)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerStay(Collider other) // 近づいたときの処理
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            
+        }
+    }
+    private void OnTriggerExit(Collider other)　// 離れた時の処理（表示を消す）
+    {
+        if (other.CompareTag("Player"))
+        {
+
+
+        }
     }
 }
