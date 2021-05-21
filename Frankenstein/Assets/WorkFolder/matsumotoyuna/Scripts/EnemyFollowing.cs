738d5d630d9bbcae0f718878cfb9c30bf7a0a196
tech <tech@t>
2021-05-21 18:47:50 +0900

    敵の追従・操作切り替えなど

--- Frankenstein/Assets/WorkFolder/matsumotoyuna/Scripts/EnemyFollowing.cs
+++ Frankenstein/Assets/WorkFolder/matsumotoyuna/Scripts/EnemyFollowing.cs
@@ -5,38 +5,99 @@ using UnityEngine;
 public class EnemyFollowing : MonoBehaviour
 {
     public GameObject player;
-    //public GameObject enemy; //いらんくね？
-    public Transform target;
+    public GameObject enemy; //いらんくね？
+    //public Transform target;
     public float speed = 3.0f; //速度
     public float stopDistance; //止まるときの距離
+
+    // 追従するかどうか
+    private bool isFollowing = true;
+
+    public MoveTest mt;
+    public bool enemyMove = true;
+    private bool Jump = false;
+
+    Rigidbody2D rigid2D;
+    float jumpForce = 300.0f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Jump = false;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         //オブジェクトを探し出して取得するもの
-        player = GameObject.Find("player");
+        //player = GameObject.Find("player");
         //enemy = GameObject.Find("enemy");
+        this.rigid2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //箱を用意して、その中にY座標を入れる
-        Vector2 targetPos = target.position;
+        Vector2 targetPos = player.transform.position;
         targetPos.y = transform.position.y;
 
         //targetの方向に向く
-        transform.LookAt(target);
+        //アニメーションも同時に左右反転させたい→後ほど
+        //transform.LookAt(target);
         //(targetPos);
 
         //距離
-        float distance = Vector2.Distance(transform.position, target.position);
+        float distance = Vector2.Distance(transform.position, player.transform.position);
 
-        //if(間の距離が止まるときの距離以上なら?)
-        if (distance > stopDistance)
+        if (isFollowing)
         {
-            transform.position = Vector2.MoveTowards(transform.position,
-            new Vector2(player.transform.position.x, player.transform.position.y), speed * Time.deltaTime);
+            //if(間の距離が止まるときの距離以上なら?)
+            if (distance > stopDistance)
+            {
+                transform.position = Vector3.MoveTowards(transform.position,
+                new Vector2(player.transform.position.x, enemy.transform.position.y),
+                speed * Time.deltaTime);
+            }
+            //enemy→player
+
+            // 右
+            if (player.transform.position.x < transform.position.x)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+
+            // 左
+            else if (player.transform.position.x > transform.position.x)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+
+            if (Jump == false && Input.GetKeyDown(KeyCode.Space))
+            {
+                this.rigid2D.AddForce(transform.up * this.jumpForce);
+                Jump = !Jump;
+            }
+        }
+
+        if (enemyMove == false)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                this.transform.Translate(-0.01f, 0.0f, 0.0f);
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                this.transform.Translate(0.01f, 0.0f, 0.0f);
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+
+            if (Jump == false && Input.GetKeyDown(KeyCode.Space))
+            {
+                this.rigid2D.AddForce(transform.up * this.jumpForce);
+                Jump = !Jump;
+            }
         }
 
         //if(プレイヤーがジャンプした)
@@ -44,29 +105,66 @@ public class EnemyFollowing : MonoBehaviour
         //     エネミーもジャンプ;
         //}
 
-        //Aを押したらこのスクリプトを無効化する(追従しなくなる)
-        //Lボタンが押されたら～に変更予定
-        if (Input.GetKey(KeyCode.A))
+        // 追従の切り替え処理
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //     //GetComponent<EnemyFollowing>().enabled = false;
+        //     Following();
+        //}
+
+        // 操作の切り替え処理
+        if (Input.GetKeyDown(KeyCode.Return))
         {
-             GetComponent<EnemyFollowing>().enabled = false;
+            //GetComponent<EnemyFollowing>().enabled = false;
+            PlayerChange();
         }
+    }
+
+    // 追従の切り替え
+    public void Following()
+    {
+        isFollowing = !isFollowing;
+    }
+
+    // 操作の切り替え
+    public void PlayerChange()
+    {
+        // プレイヤーの操作をできなくする
+        mt.playerMove = !mt.playerMove;
+
+        // 操作権を敵に移動させる
+        Following();
+        enemyMove = !enemyMove;
+
+
+        //この状態で別のボタンを押すと、操作切り替え・追従なし
+        /*if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+           mt.playerMove = !mt.playerMove;
+        }*/
+
+        // カメラの追従を敵に移す
     }
 }
 
-/*
+/*解決
 敵の追従
 プレイヤーの座標と重なってはいけない！
 プレイヤーとエネミーは、いくらかの間を開けるイメージ
 進行方向と反対側につく？(プレイヤーが右に進んでいる時は左側につく)
-enemyがしょっちゅう消えてしまうのですが、これは…
+enemyが消えてしまうのですが、これは…
 */
 
 /*
 Lボタンで操作切り替え
-そのあとは付いてこない→機能オフ
+そのあとは付いてこない→機能オフ?
 */
 
 /*
 Lボタンを押した後にAボタンを押すと呼べる→機能オン
 (これはいらなさそう？)
+*/
+
+/*
+足の上げ下げアニメーション
 */
--- Frankenstein/Assets/WorkFolder/matsumotoyuna/Scripts/MoveTest.cs
+++ Frankenstein/Assets/WorkFolder/matsumotoyuna/Scripts/MoveTest.cs
@@ -4,22 +4,48 @@ using UnityEngine;
 
 public class MoveTest : MonoBehaviour
 {
+    public GameObject player;
+
+    public bool playerMove = false;
+    private bool Jump = false;
+
+    Rigidbody2D rigid2D;
+    float jumpForce = 300.0f;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Jump = false;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        this.rigid2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        //isControlChange = !isControlChange;
+
+        if (playerMove == false)
         {
-            this.transform.Translate(-0.01f, 0.0f, 0.0f);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            this.transform.Translate(0.01f, 0.0f, 0.0f);
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                this.transform.Translate(-0.01f, 0.0f, 0.0f);
+                transform.localScale = new Vector3(-0.5f, 0.4710938f, 1);
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                this.transform.Translate(0.01f, 0.0f, 0.0f);
+                transform.localScale = new Vector3(0.5f, 0.4710938f, 1);
+            }
+
+            if (Jump == false && Input.GetKeyDown(KeyCode.Space))
+            {
+                this.rigid2D.AddForce(transform.up * this.jumpForce);
+                Jump = !Jump;
+            }
         }
     }
 }
