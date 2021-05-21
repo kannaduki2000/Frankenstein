22361ae110a04f541e59ddfaf883cef8e0013117
tech <tech@tech>
2021-05-21 18:46:26 +0900

    電流関係

--- Frankenstein/Assets/WorkFolder/yoshidatomoya/Scripts/ElectricCurrent.cs
+++ Frankenstein/Assets/WorkFolder/yoshidatomoya/Scripts/ElectricCurrent.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class ElectricCurrent : MonoBehaviour
 {
+    // HPを表示
+    int HP = 100;
 
     public float speed;
     private Rigidbody2D rb;
+
+    private bool touchFlag = false;
+
+    public GameObject hpBar;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -17,7 +25,7 @@ public class ElectricCurrent : MonoBehaviour
         // プレイヤー移動
         float horizontalKey = Input.GetAxis("Horizontal");
 
-        if(horizontalKey > 0)
+        if (horizontalKey > 0)
         {
             rb.velocity = new Vector2(speed, rb.velocity.y);
         }
@@ -31,26 +39,69 @@ public class ElectricCurrent : MonoBehaviour
         }
     }
 
+
     // Update is called once per frame
     void Update()
     {
+        if (touchFlag)
+        {
+            Debug.Log("aaa");
+            // 表示
+            hpBar.SetActive(true);
+
+            // 電気を流す
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                // HPを減らす
+                HP -= 30;
+                Debug.Log(HP);
+                // ここに処理を加える
+            }
+            // 電気を充電
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // HPを減らす
+                HP += 30;
+                Debug.Log(HP);
+                // ここに処理を加える
+            }
+
+
+        }
+
+
+        // 非表示
 
     }
 
-    private void OnTriggerStay(Collider other) // 近づいたときの処理
-    {
-        if (other.CompareTag("Player"))
-        {
 
-            
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "HomeApp")
+        {
+            touchFlag = true;   
         }
     }
-    private void OnTriggerExit(Collider other)　// 離れた時の処理（表示を消す）
+
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        if (other.CompareTag("Player"))
+        if (collision.gameObject.tag == "HomeApp")
         {
-
-
+            touchFlag = false;
+            hpBar.SetActive(false);
         }
     }
 }
+
+/*
+＠電流関係
+    電気を流す〇
+　　電気を充電する〇
+　　電気を流せるかどうかの判定
+　　　流せるなら流せる対象の状態（電気を流すor充電）を取得〇
+　　 　種類に合わせた処理をする
+　　　　（敵ならその敵に応じた電気を消費して追従）
+　　　　（ギミックならそのギミックに応じた処理）
+　　電気を流せる物の近くに立ったらHPバーとボタンの表示 〇
+*/
